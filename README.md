2f0ac4cc31355cd9d0997323d7e7b50d30f5990a
Johnny Rice <jrice@labact.com>
2020-07-23 18:29:34 -0400

    Provide week 08 classwork

---
+++ README.md
@@ -0,0 +1,19 @@
+# Thursday night Hoorah
+
+Week 07.five
+
+- More VS Code extensions!
+-- http/rest - <https://marketplace.visualstudio.com/items?itemName=humao.rest-client>
+-- better console log - <https://marketplace.visualstudio.com/items?itemName=ChakrounAnas.turbo-console-log>
+
+- Array Reduce
+
+Week 09 Prep
+
+Back to npm and node and modules!
+
+- Express
+
+- HTTP and REST
+
+Week 07 Classwork Assignment and Homework
---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+
+    We will push to github all along the way so we track our week and keep a log
+
+    Fork
+    Clone to local machine
+    First step on local: `git checkout -b answer`
+
+    Declaring an Array with Objects
+    and
+    Referencing code in other files
+
+    1)  Please read all instructions first!
+
+        Create a new file named "grades.js" to import in the data folder
+        
+        Declare and initialize an array named `grades`
+    
+        Include at least 5 object items in the array 
+        
+        Each object will have properties of
+            - `assignmentName`  with string values such as "Assignment 01"
+            - `studentName` with string values such as "Tito"
+            - `score` with numeric values such as 50
+
+        Values are important for this lesson.
+
+            - Use 2 distinct assignment names
+            - Use 2 distinct student names
+
+ */
+
+ /**
+
+    Looping using your preferred looping syntax and updating a shared variable
+ 
+    2) Loop through the grades data using a for loop.
+        Update `gradeTotal` so it increases value for each item in the array
+
+ */
+ const gradeTotal = 0
+
+
+ /**
+ 
+    Using reduce
+
+    3) Use Array reduce to do the same total calculation logic
+
+        Replace `null` below with the use of `reduce`
+ 
+  */
+  gradeTotal = null
