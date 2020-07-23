e6209671d4e220f0d06d95dbab6801828b036005
Sasha Gemis <gemissasha@gmail.com>
2020-07-23 19:10:13 -0400

    sasha's sample data

---
+++ grades/grades.js
@@ -0,0 +1,32 @@
+const grades = [
+    {assignmentName: 'in the weeds',
+    studentName: 'Mahla Jane',
+    grade: 80} ,
+
+    {assignmentName: "betty's garden",
+    studentName: 'Sasha Gemis',
+    grade: 85},
+
+    {assignmentName: 'stolen lullabies',
+    studentName: 'Mahla Jane',
+    grade: 90},
+
+    {assignmentName: 'hide-and-seek',
+    studentName: 'Sasha Gemis',
+    grade: 75},
+
+    {assignmentName: 'running like water',
+    studentName: 'Mahla Jane',
+    grade: 70},
+
+    {assignmentName: 'clandestine meetings',
+    studentName: 'Sasha Gemis',
+    grade: 95},
+
+    {assignmentName: 'meet me behind the mall',
+    studentName: 'Mahla Caram',
+    grade: 100}]
+  
+module.exports = {
+    grades
+}
